ac18cf1350a4a5f4b37d109570f04f43f23a3c23
kdaud <dkakumirizii@gmail.com>
2020-01-06 21:04:38 +0300

    files added

--- src/com/opensource/go/JabRef.java
+++ src/com/opensource/go/JabRef.java
@@ -1,6 +1,9 @@
 package com.opensource.go;
 
 public class JabRef {
+	static {
+		System.out.println("Eyeing at GSoC 2020");
+	}
 	public static void main(String [] args) {
 	
 System.out.println(sayHelloWorld());
